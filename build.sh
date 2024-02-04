2851b322a9c880eea0ec943d85829d5b1f047697
Begerad, Stefan <dialog@swingbe.de>
2024-02-04 07:57:32 +0100

    feat: swap sponge CLI with tee CLI

--- build.sh
+++ build.sh
@@ -21,10 +21,10 @@ NODE_ENV=production node_modules/.bin/gtfs-to-sql -d --trips-without-shape-id --
 	gtfs/stops.csv \
 	gtfs/transfers.csv \
 	gtfs/trips.csv \
-	| sponge | psql -b
+	| tee | psql -b
 	# gtfs/shapes.csv \
 
 lib="$(dirname $(realpath $0))/lib"
 NODE_ENV=production node_modules/.bin/build-gtfs-match-index \
 	$lib/hafas-info.js $lib/gtfs-info.js \
-	| sponge | psql -b
+	| tee | psql -b
